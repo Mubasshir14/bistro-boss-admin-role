62d630ea67042b49751bbe8b31c9b73a9eb248c7
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-07-04 01:37:38 +0600

    Authentication

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,10 +1,22 @@
 
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+    const [isOpen, setIsOpen] = useState(false);
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
 
+            })
+            .then(error => {
+                console.log(error.message);
+            })
+    }
     const navOptions =
         <>
             <li><Link className="" to='/'>HOME</Link></li>
@@ -12,6 +24,7 @@ const NavBar = () => {
             <li><Link to='/menu'>OUR MENU</Link></li>
             <li><Link to='/order/salad'>ORDER FOOD</Link></li>
             <li><Link to='/secret'>SECRET</Link></li>
+
             {/* <li>
                 <Link to='/'>
                     <button className="btn">
@@ -48,7 +61,65 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    {/* {
+                        user ? <>
+                            <li><span>{user?.displayName}</span></li>
+                            <li><img className="rounded-full w-20 h-20" src={user?.photoURL} alt="" /></li>
+                            <button onClick={handleLogOut} className="btn btn-ghost">LOG OUT</button></> : <><li><Link className="btn" to='/login'>LOG IN</Link></li></>
+                    } */}
+                    {
+                        user ? <>
+                            <div className="relative inline-block">
+                                {/* Dropdown toggle button */}
+                                <button
+                                    onClick={() => setIsOpen(!isOpen)}
+                                    className="relative z-10 flex items-center p-2 text-sm  border border-transparent rounded-md focus:border-blue-500 focus:ring-opacity-40 dark:focus:ring-opacity-40 focus:ring-blue-300 dark:focus:ring-blue-400 focus:ring text-white bg-[#CD9003] focus:outline-none"
+                                >
+                                    <span className="mx-1">View Profile</span>
+                                    <svg className="w-5 h-5 mx-1" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 15.713L18.01 9.70299L16.597 8.28799L12 12.888L7.40399 8.28799L5.98999 9.70199L12 15.713Z" fill="currentColor"></path>
+                                    </svg>
+                                </button>
+
+                                {/* Dropdown menu */}
+                                {isOpen && (
+                                    <div
+                                        onClick={() => setIsOpen(false)}
+                                        className="absolute right-0 z-20 w-56 py-2 mt-2 overflow-hidden origin-top-right bg-white rounded-md shadow-xl dark:bg-gray-800"
+                                    >
+                                        <Link
+                                           
+                                            className="flex items-center p-3 -mt-2 text-sm text-gray-600 transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                                        >
+                                            <img
+                                                className="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9"
+                                                src={user?.photoURL}
+                                                alt="jane avatar"
+                                            />
+                                            <div className="mx-1">
+                                                <h1 className="text-sm font-semibold text-gray-700 dark:text-gray-200">{user?.displayName}</h1>
+                                                <p className="text-xs text-gray-500 dark:text-gray-400">{user.email}</p>
+                                            </div>
+                                        </Link>
+
+                                        <hr className="border-gray-200 dark:border-gray-700" />
+                                        {/* <p
+                                           
+                                            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                                        >
+                                            {user?.displayName}
+                                        </p> */}
+                                        <p
+                                            onClick={handleLogOut}
+                                            className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+                                        >
+                                            Sign Out
+                                        </p>
+                                    </div>
+                                )}
+                            </div>
+                        </> : <><li><Link className="btn" to='/login'>LOG IN</Link></li></>
+                    }
                 </div>
             </div>
         </>
