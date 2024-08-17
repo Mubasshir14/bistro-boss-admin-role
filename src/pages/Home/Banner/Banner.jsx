1067fd472936bf85bc560192c78b97c31fd73e4d
Mubasshir Al Kasshaf <mubasshiralkasshaf22@gmail.com>
2024-08-17 17:55:11 +0600

    Banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,7 +1,6 @@
-// import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './Banner.css';  
 import img1 from '../../../assets/home/01.jpg'
 import img2 from '../../../assets/home/02.jpg'
 import img3 from '../../../assets/home/03.png'
@@ -9,34 +8,34 @@ import img4 from '../../../assets/home/04.jpg'
 import img5 from '../../../assets/home/05.png'
 import img6 from '../../../assets/home/06.png'
 
-
-
 const Banner = () => {
     return (
-        <Carousel>
+        <Carousel 
+            className="custom-carousel"  
+            showArrows={true} 
+            autoPlay={true} 
+            infiniteLoop={true} 
+            showThumbs={true} 
+            showStatus={false} 
+            interval={3000}
+        >
             <div>
-                <img src={img2} />
-                {/* <p className="legend">Legend 1</p> */}
+                <img src={img1} alt="Image 1" />
             </div>
             <div>
-                <img src={img1} />
-                
+                <img src={img2} alt="Image 2" />
             </div>
             <div>
-                <img src={img3} />
-                
+                <img src={img3} alt="Image 3" />
             </div>
             <div>
-                <img src={img4} />
-               
+                <img src={img4} alt="Image 4" />
             </div>
             <div>
-                <img src={img5} />
-              
+                <img src={img5} alt="Image 5" />
             </div>
             <div>
-                <img src={img6} />
-                
+                <img src={img6} alt="Image 6" />
             </div>
         </Carousel>
     );
